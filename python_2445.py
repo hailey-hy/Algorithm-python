b22bb97f354a8529e176e8caa686b1b2e87a58de
hailey-hy <hailey.hy@gmail.com>
2021-12-27 22:04:01 +0900

    백준 2445

---
+++ python_2445.py
@@ -0,0 +1,8 @@
+# 예제대로 별 찍기
+
+n = int(input())
+
+for i in range(1, n + 1):
+    print("*" * i + " " * (n - i) + " " * (n - i) + "*" * i)
+for i in range(1, n):
+    print("*" * (n - i) + " " * i + " " * i + "*" * (n - i))
