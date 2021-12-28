6a9d8eb9fafda667cdf76eb5773973bc6de349e4
hailey-hy <lillyine.hy@gmail.com>
2021-12-28 23:43:19 +0900

    백준 2775

--- python_2775.py
+++ python_2775.py
@@ -1,18 +1,16 @@
+# a층의 b호에 살려면 자신의 아래(a-1)층의 1호부터 b호까지 사람들의 수의 합만큼 사람들을 데려와 살아야할 경우,
+# 각각의 Test caseㅔ 대해 해당 집의 거주민 수를 출력하라
+
 case_num = int(input())
-floor = int(input())
-room = int(input())
-total = 0
-
-for i in range(case_num):
-    x = []
-    first_floor = []
-    for j in range(1, floor + 1):
-        first_floor.append(j)
-    for k in range(1, room + 1):
-        x.append()
 
 
-
-# 1 4 10 20 25
-# 1 3 6 10 15
-# 1 2 3 4 5
+for i in range(case_num): 
+    ground_floor = []
+    floor = int(input())
+    room = int(input())
+    for j in range(1, room + 1): 
+        ground_floor.append(j)
+    for j in range(1, floor + 1): 
+        for k in range(1, room):
+            ground_floor[k] += ground_floor[k - 1]
+    print(ground_floor[room - 1])
