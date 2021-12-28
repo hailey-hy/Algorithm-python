c72e84d6a291edbe365059c6dfefb28592582b5f
hailey-hy <lillyine.hy@gmail.com>
2021-12-28 23:04:27 +0900

    백준 2775

---
+++ python_2775.py
@@ -0,0 +1,18 @@
+case_num = int(input())
+floor = int(input())
+room = int(input())
+total = 0
+
+for i in range(case_num):
+    x = []
+    first_floor = []
+    for j in range(1, floor + 1):
+        first_floor.append(j)
+    for k in range(1, room + 1):
+        x.append()
+
+
+
+# 1 4 10 20 25
+# 1 3 6 10 15
+# 1 2 3 4 5
