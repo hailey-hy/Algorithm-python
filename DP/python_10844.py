4ccf4787a296d9d864c79bb324f551f60c9db722
hailey-hy <lillyine.hy@gmail.com>
2022-01-02 19:56:21 +0900

    백준 10844

---
+++ DP/python_10844.py
@@ -0,0 +1,16 @@
+# 계단 수
+# 각 자리수마다 가능한 경우의 수를 2차원 배열에 구현한 후 다이나믹 프로그래밍으로 해결
+
+n = int(input())
+dp = [[0 for i in range(10)] for j in range(101)]
+for i in range(1, 10):
+    dp[1][i] = 1
+for i in range(2, n + 1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i - 1][1]
+        elif j == 9:
+            dp[i][j] = dp[i - 1][8]
+        else:
+            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
+print(sum(dp[n]) % 1000000000)
