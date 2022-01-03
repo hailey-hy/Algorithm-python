f57e83bd54ca0e0eca67ca3603d29f8e5e5c42d9
hailey-hy <lillyine.hy@gmail.com>
2022-01-03 23:08:41 +0900

    DP를 활용한 알고리즘 문제 풀이

---
+++ DP/python_11057.py
@@ -0,0 +1,19 @@
+import sys
+
+n = int(sys.stdin.readline())
+total = 0
+d = [[0 for i in range(10)] for j in range(n + 1)]
+
+for i in range(10):
+    d[1][i] = 1
+
+for i in range(2, n + 1):
+    for j in range(10):
+        if j > 1:
+            d[i][j] = d[i - 1][j] + d[i][j - 1]
+        elif j == 1:
+            d[i][j] = 1
+        else:
+            d[i][j] = sum(d[i - 1])
+
+print(sum(d[n]))
