884d70b209ddd93f5a8c9cb24a7047f95a030255
hailey-hy <lillyine.hy@gmail.com>
2022-02-09 00:08:09 +0900

    하노이의 탑

--- etc/python_11729.py
+++ etc/python_11729.py
@@ -1,32 +1,16 @@
 # 하노이의 탑
 
-from pickle import APPEND
-
-
-def hanoi(n):
-    stack1 = [i for i in range(1, n + 1)]
-    if len(stack2) == 0 and len(stack3) == 0:
-        out = stack1.pop()
-        stack3.append(out)
-        out = stack1.pop()
-        stack2.append(out)
-        n -= 2
-        return hanoi(n)
-    elif len(stack2) != 0 or len(stack3) != 0:
-        if stack2[-1] > stack3[-1]:
-            out = stack3.pop()
-            stack2.append(out)
-            return hanoi(n)
-        else:
-            out = stack2.pop()
-            stack3.append(out)
-            return hanoi(n)
-
-    elif len(stack1) == 0 and len(stack2) == 0 and stack3 == sorted(stack3): 
-        return 
-        
-
-
 n = int(input())
-stack2 = []
-stack3 = []
+
+def hanoi(n, start, middle, end):
+    if n == 1:
+        print(start, end)
+    else:
+        hanoi(n - 1, start, end, middle)
+        print(start, end)
+        hanoi(n - 1, middle, start, end)
+sum = 1
+for i in range(n - 1):
+    sum = sum * 2 + 1
+print(sum)
+hanoi(n, 1, 2, 3)
