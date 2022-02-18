22d2613e1a7969c8f90515ed02cc61ab90aa1002
hailey-hy <lillyine.hy@gmail.com>
2022-02-18 14:27:13 +0900

    피보나치

--- DP/python_1003.py
+++ DP/python_1003.py
@@ -3,14 +3,13 @@
 
 n = int(input())
 
-d_0 = [1, 0]
-d_1 = [0, 1]
-
-
-
 for i in range(n):
     num = int(input())
-    for j in range(2, num + 1):
-        d_0.append(d_0[j - 2] + d_0[j - 1])
-        d_1.append(d_1[j - 2] + d_1[j - 1])
-    print(d_0[num], d_1[num])
+    zero = 1
+    one = 0
+    cnt = 0
+    for _ in range(num):
+        cnt = one
+        one = one + zero
+        zero = cnt
+    print(zero, one)
