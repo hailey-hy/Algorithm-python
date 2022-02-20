67a287310b35da7d5f0d4dd7fa84a30e5c2a815b
hailey-hy <lillyine.hy@gmail.com>
2022-02-20 16:38:05 +0900

    RGB 거리

---
+++ DP/python_1149.py
@@ -0,0 +1,15 @@
+# RGB 거리
+
+t = int(input())
+
+cost = []
+
+for i in range(t):
+    cost.append(list(map(int, input().split())))
+
+for i in range(1, t):
+    cost[i][0] = min(cost[i - 1][1], cost[i - 1][2]) + cost[i][0]
+    cost[i][1] = min(cost[i - 1][0], cost[i - 1][2]) + cost[i][1]
+    cost[i][2] = min(cost[i - 1][0], cost[i - 1][1]) + cost[i][2]
+
+print(min(cost[t - 1][0], cost[t - 1][1], cost[t - 1][2]))
