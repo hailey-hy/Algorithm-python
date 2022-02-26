c296dbb1f67e11057749349139b4d868b2039f7d
hailey-hy <lillyine.hy@gmail.com>
2022-02-26 21:16:37 +0900

    Longest Common Subsequence

---
+++ DP/python_9251.py
@@ -0,0 +1,16 @@
+# Longest Common Subsequence
+
+a = input()
+b = input()
+
+
+d = [[0] * (len(b) + 1) for _ in range(len(a) + 1)]
+
+for i in range(1, len(a) + 1):
+    for j in range(1, len(b) + 1):
+        if a[i - 1] == b[j - 1]:
+            d[i][j] = d[i - 1][j - 1] + 1
+        else:
+            d[i][j] = max(d[i - 1][j], d[i][j - 1])
+
+print(d[-1][-1])
