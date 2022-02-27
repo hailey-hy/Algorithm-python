6d643c42c0e102de03d5f6da1adac654d6af12d3
hailey-hy <lillyine.hy@gmail.com>
2022-02-28 01:15:35 +0900

    평범한 배낭

--- DP/python_12865.py
+++ DP/python_12865.py
@@ -5,7 +5,20 @@ from array import array
 
 n, k = map(int, input().split())
 
-array = [list(map(int, input().split())) for i in range(n)]
+array = [[0, 0]]
+for i in range(n):
+    w, v = map(int, input().split())
+    array.append([w, v])
 
+d = [[0] * (k + 1) for _ in range(n + 1)]
 
-print(array)
+for i in range(1, n + 1):
+    w = array[i][0]
+    v = array[i][1]
+    for j in range(1, k + 1):
+        if j < w:
+            d[i][j] = d[i - 1][j]
+        else:
+            d[i][j] = max(d[i - 1][j - w] + v, d[i - 1][j])
+
+print(d[n][k])
