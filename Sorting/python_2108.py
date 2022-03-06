1d30128b29c9cbf2317613fccc1b60d349eee84e
hailey-hy <lillyine.hy@gmail.com>
2022-03-06 21:32:18 +0900

    통계학

--- Sorting/python_2108.py
+++ Sorting/python_2108.py
@@ -1,7 +1,26 @@
 # 통계학
 
-n = int(input())
+import sys
+from collections import Counter
+
+n = int(sys.stdin.readline())
 
 array = [int(input()) for i in range(n)]
 
-A = round(sum(array), 1)
+array.sort()
+
+total = sum(array)
+if total >= 0:
+    print(int(total/n + 0.5))
+else:
+    print(int(total/n - 0.5))
+
+print(array[n//2])
+
+cnt = Counter(array).most_common()
+if len(cnt) > 1 and cnt[0][1] == cnt[1][1]:
+    print(cnt[1][0])
+else:
+    print(cnt[0][0])
+
+print(array[-1] - array[0])
