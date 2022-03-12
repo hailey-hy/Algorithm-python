2005d422271b421bb2d538a493e078bebbbb8404
hailey-hy <lillyine.hy@gmail.com>
2022-03-12 18:51:16 +0900

    괄호

--- Stack/python_9012.py
+++ Stack/python_9012.py
@@ -2,27 +2,22 @@
 
 import sys
 
-n = int(sys.stdin.readline())
+T = int(sys.stdin.readline())
 
-def VPS(stack):
-    if len(stack) = 0:
-        return "YES"
-    elif len(stack) == 1 and stack[0] == "(":
-        stack.pop()
-        return "YES"
-    elif stack[-1] == ")":
-        stack.pop()
-        for k in range(len(stack)):
-            if stack[-1] == "(":
-                stack.pop()
-                VPS(stack)
-            else:
-                stack.pop()
-    else:
-        return "NO"
-
-for i in range(n):
+for _ in range(T):
+    D = sys.stdin.readline().strip()
     stack = []
-    for j in input():
-        stack.append(j)
-    print(VPS(stack))
+    for p in D:
+        if p == '(':
+            stack.append(p)
+        else:
+            if len(stack):
+                stack.pop()
+            else:
+                print('NO')
+                break
+    else:
+        if stack:
+            print('NO')
+        else:
+            print('YES')
