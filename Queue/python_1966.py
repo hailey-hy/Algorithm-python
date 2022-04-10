edd67a15e0bcd1c1e2c450eb4c9ebd0f6a7cb49a
전해연 <lillyine.hy@gmail.com>
2022-04-11 03:08:42 +0900

    프린터 큐

--- Queue/python_1966.py
+++ Queue/python_1966.py
@@ -1,3 +1,25 @@
 # 프린터 큐
 
-..
+import sys
+from collections import deque
+
+T = int(sys.stdin.readline())
+
+for i in range(T):
+    n, m = map(int, sys.stdin.readline().split())
+    array = list(map(int, sys.stdin.readline().split()))
+    array_priorities = [i + 1 for i in range(n)]
+    array_printed = []
+    target = array_priorities[m]
+    while array:
+        first = array.pop(0)
+        if (len(array) == 0) or (first >= max(array)):
+            array_printed.append(first)
+            if target == array_priorities[0]:
+                print(len(array_printed))
+                break
+            else:
+                array_priorities.pop(0)
+        else:
+            array.append(first)
+            array_priorities.append(array_priorities.pop(0))
