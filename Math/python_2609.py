167be453df8ea957f75b4d4d0346f0164aaa94b5
hailey-hy <lillyine.hy@gmail.com>
2022-08-17 20:35:16 +0900

    최대공약수와 최소공배수

--- Math/python_2609.py
+++ Math/python_2609.py
@@ -1,9 +1,9 @@
 # 최대공약수와 최소공배수
 
 a, b = map(int, input().split())
-deno = 0
+deno = 1
 
-for i in range(1, a // 2 + 1):
+for i in range(1, a + 1):
     if a % i == 0 and b % i == 0:
         deno = i
 
