45db656ca8c70c6c11aa7d9bcfa45e9831313af4
hailey-hy <lillyine.hy@gmail.com>
2022-09-01 01:32:24 +0900

    N과 M (2)

---
+++ Back_Tracking/python_15650.py
@@ -0,0 +1,18 @@
+# N과 M (2)
+
+def dfs():
+    if len(stack) == m:
+        print(' '.join(map(str, stack)))
+        return
+    
+    for i in range(1, n + 1):
+        if i in stack or (len(stack) >= 1 and stack[-1] > i):
+            continue
+
+        stack.append(i)
+        dfs()
+        stack.pop()
+
+n, m = map(int, input().split())
+stack = []
+dfs()
