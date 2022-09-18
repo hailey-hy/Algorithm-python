7e09b3bc06bdaaf584685585f0015d2413e133c5
hailey-hy <lillyine.hy@gmail.com>
2022-09-18 21:57:54 +0900

    회전하는 큐

--- Queue/python_1021.py
+++ Queue/python_1021.py
@@ -1,15 +1,27 @@
 # 회전하는 큐
 
-import sys
-
-n, m = map(int, sys.stdin.readline().split())
-targets = list(map(int, sys.stdin.readline().split()))
-array = [i + 1 for i in range(n)]
-trial = 0
-
-while targets:
-    if array[0] == targets[0]:
-        targets.pop(0)
-        array.append(array.pop(0))
-    elif array.index(targets[0]) < (n // 2):
+from collections import deque
         
+n, m = map(int, input().split())
+
+q = deque([i for i in range(1, n + 1)])
+
+nums = list(map(int, input().split()))
+
+total = 0
+
+for i in nums:
+    while m > 0:
+        if q[0] == i:
+            q.popleft()
+            break
+        else:
+            total += 1
+            if q.index(i) >= len(q)//2 + 1:
+                q.appendleft(q.pop())
+            else:
+                q.append(q.popleft())
+    
+    m -= 1
+
+print(total)
