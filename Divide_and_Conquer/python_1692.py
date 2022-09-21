79fb99c59a4db1ad5ce516e24c8de83cd6e4e26c
hailey-hy <lillyine.hy@gmail.com>
2022-09-21 23:32:59 +0900

    곱셈

---
+++ Divide_and_Conquer/python_1692.py
@@ -0,0 +1,16 @@
+# 곱셈
+import sys
+sys.setrecursionlimit(10 ** 6)
+a, b, c = map(int, input().split())
+
+def multi (a,n):
+  if n == 1:
+      return a%c
+  else:
+      tmp = multi(a,n//2)
+      if n % 2 == 0:
+          return (tmp * tmp) % c
+      else:
+          return (tmp  * tmp *a) %c
+          
+print(multi(a,b))
