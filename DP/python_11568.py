6a4a54c5c55dcf60ffbae93ee5349e267ea534ee
hailey-hy <lillyine.hy@gmail.com>
2022-10-12 16:50:34 +0900

    민균이의 계략

--- DP/python_11568.py
+++ DP/python_11568.py
@@ -12,7 +12,8 @@ for i in range(1,n):
     max_value = 0
     for j in range(i):
         if card[j] < card[i]:
-            max_value = max(max_value, dp[j])
+            max_value = max(max_value, dp[j]) #검증 대상(card[j])의 증가하는 순열 기록에 +1하기 위해 dp[j]를 사용
+            # max_value = max_value + 1
         
     dp[i] = max_value + 1
 
