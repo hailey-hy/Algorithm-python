2f468d9e73334cbe99f627f7409060efee2d6a4a
hailey-hy <lillyine.hy@gmail.com>
2022-10-21 00:34:41 +0900

    점프

--- DP/python_1890.py
+++ DP/python_1890.py
@@ -6,7 +6,7 @@ game = []
 for _ in range(n):
     game.append(list(map(int, input().split())))
 
-dp = [[0] * (n + 1) for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
 
 dp[0][0] = 1
 
@@ -14,8 +14,18 @@ for i in range(n):
     for j in range(n):
         if game[i][j] > 0 and dp[i][j] > 0:
             if i + game[i][j] < n:
-                dp[i + game[i][j]][j] += 1
+                tmp = True
+                for k in range(1, game[i][j] + 1):
+                    if game[i + k][j] == 0 and not(i  + k== n - 1 and j == n - 1):
+                        tmp = False
+                if tmp == True or i == n - 2 and j == n - 1:
+                    dp[i + game[i][j]][j] += dp[i][j]
             if j + game[i][j] < n:
-                dp[i][j + game[i][j]] += 1
+                tmp = True
+                for k in range(1, game[i][j] + 1):
+                    if game[i][j + k] == 0 and not(i == n - 1 and j + k == n - 1):
+                        tmp = False
+                if tmp == True or i == n - 1 and j == n - 2:
+                    dp[i][j + game[i][j]] += dp[i][j]
 
 print(dp[n - 1][n - 1])
