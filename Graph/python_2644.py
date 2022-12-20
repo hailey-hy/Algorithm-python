270922cf95afe0813332ac3c7784a422f645290b
hailey-hy <lillyine.hy@gmail.com>
2022-12-20 16:36:54 +0900

    촌수 계산

--- Graph/python_2644.py
+++ Graph/python_2644.py
@@ -1,2 +1,29 @@
 # 촌수계산
 
+from collections import deque
+
+n = int(input())
+family = [[] for _ in range(n + 1)]
+
+x, y = map(int, input().split())
+
+relations = int(input())
+for _ in range(relations):
+    target, related = map(int, input().split())
+    family[target].append(related)
+    family[related].append(target)
+
+visited = [-1 for _ in range(n + 1)]
+visited[x] = 0
+
+def bfs(a):
+    queue = deque([a])
+    while queue:
+        now = queue.popleft()
+        for i in family[now]:
+            if visited[i] == -1:
+                visited[i] = visited[now] + 1
+                queue.append(i)
+
+bfs(x)
+print(visited[y])
