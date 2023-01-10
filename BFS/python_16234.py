0a11164ae5132682f5547a2989c8b674201f3b67
hailey-hy <lillyine.hy@gmail.com>
2023-01-11 00:57:55 +0900

    인구 이동

--- BFS/python_16234.py
+++ BFS/python_16234.py
@@ -1,59 +1,43 @@
-# 인구 이동
-
 from collections import deque
-
-n, L, R = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-answer = 0
-
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-def bfs():
-    ally = 0
-    ally_arry = []
-    memo = [[-1] * n for _ in range(n)]
-    tmp = False
+import sys
+input = sys.stdin.readline
+def bfs(i, j):
+    q = deque()
+    q.append([i, j])
+    temp = []
+    temp.append([i, j])
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n and visit[nx][ny] == 0:
+                if l <= abs(s[nx][ny] - s[x][y]) <= r:
+                    visit[nx][ny] = 1
+                    q.append([nx, ny])
+                    temp.append([nx, ny])
+    return temp
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
+n, l, r = map(int, input().split())
+s = []
+for i in range(n):
+    s.append(list(map(int, input().split())))
+cnt = 0
+while True:
+    visit = [[0] * n for i in range(n)]
+    isTrue = False
     for i in range(n):
         for j in range(n):
-            if memo[i][j] == -1:
-                q = deque([(i, j)])
-                visited = [[False] * n for _ in range(n)]
-                visited[i][j] = True
-                memo[i][j] = ally
-                total = 0
-                num = 0
-                while q:
-                    x, y = q.popleft()
-                    total += graph[x][y]
-                    num += 1
-                    for k in range(4):
-                        new_x = dx[k] + x
-                        new_y = dy[k] + y
-                        if 0 <= new_x < n and 0 <= new_y < n:
-                            if L <= abs(graph[x][y] - graph[new_x][new_y]) <= R and not visited[new_x][new_y]:
-                                visited[new_x][new_y] = True
-                                memo[new_x][new_y] = ally
-                                q.append((new_x, new_y))
-                                tmp = True
-                ally += 1
-                ally_arry.append(int(total / num))
-
-    if tmp:
-        for i in range(n):
-            for j in range(n):
-                if memo[i][j] > -1:
-                    graph[i][j] = ally_arry[memo[i][j]]
-        return True
-
-    else:
-        return False
-    
-
-while True:
-    if not bfs():
+            if visit[i][j] == 0:
+                visit[i][j] = 1
+                temp = bfs(i, j)
+                if len(temp) > 1:
+                    isTrue = True
+                    num = sum([s[x][y] for x, y in temp]) // len(temp)
+                    for x, y in temp:
+                        s[x][y] = num
+    if not isTrue:
         break
-    else:
-        answer += 1
-
-print(answer)
+    cnt += 1
+print(cnt)
